122ef7657d9cc56860ca618572f4c7829f3c39f5
Adrien Denat <adrien.denat@gmail.com>
2020-12-24 00:36:37 +0000

    chore(core): add stencil components

---
+++ packages/core/src/components/slide/slide.tsx
@@ -0,0 +1,11 @@
+import { h, Component, Host } from '@stencil/core';
+
+@Component({
+  tag: 'snap-carousel-slide',
+  styleUrl: 'slide.scss',
+})
+export class Slide {
+  render() {
+    return <Host />;
+  }
+}
---
+++ packages/core/src/components/slides/slides.tsx
@@ -0,0 +1,11 @@
+import { h, Component, Host } from '@stencil/core';
+
+@Component({
+  tag: 'snap-carousel-slides',
+  styleUrl: 'slides.scss',
+})
+export class Slides {
+  render() {
+    return <Host />;
+  }
+}
