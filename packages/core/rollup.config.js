f4917bcd59e7d5ba300ee8693192506a6b8f20b1
Adrien Denat <adrien.denat@gmail.com>
2022-06-15 12:29:07 +0100

    chore: remove hack for compiling TS types and use recommended workaround for Rollup instead
    https://github.com/rollup/plugins/tree/master/packages/typescript#declaration-output-with-outputfile

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,25 +1,30 @@
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import external from "rollup-plugin-peer-deps-external";
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
+      format: 'cjs',
+      exports: 'named',
       sourcemap: true,
     },
     {
       file: pkg.module,
-      format: "es",
-      exports: "named",
+      format: 'es',
+      exports: 'named',
       sourcemap: true,
     },
   ],
-  plugins: [external(), resolve(), typescript(), commonjs()],
+  plugins: [
+    external(),
+    resolve(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    commonjs(),
+  ],
 };
