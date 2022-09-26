ec30ad10b8d2495524c14a2b812c8456b92c4341
Adrien Denat <oi@adriendenat.com>
2022-09-26 21:03:16 +0100

    feat(core): rework active snap observers to compute snap-align per item instead of globally

--- packages/core/src/getActiveSnap.ts
+++ packages/core/src/getActiveSnap.ts
@@ -3,10 +3,8 @@ import { debounceHOF } from './utils';
 export const getActiveSnap = ({
   root,
   onChange,
-  snapAlign = 'start'
 }: {
   root: HTMLDivElement;
-  snapAlign?: 'start' | 'center' | 'end';
   onChange?: (snapIndex: number) => void;
 }) => {
   let activeSnapObserver: IntersectionObserver;
@@ -48,131 +46,153 @@ export const getActiveSnap = ({
     if (timeout) clearTimeout(timeout);
 
     timeout = setTimeout(() => {
-      // if (root.scrollLeft === 0) {
-      //   setSnapIndex(0);
-      // } else if (root.scrollLeft === root.scrollWidth - root.offsetWidth) {
-      //   setSnapIndex(children.length - 1);
-      // }
       isScrolling = false;
+      if (root.scrollLeft === 0) {
+        setSnapIndex(0);
+      }
     }, 50) as any;
   };
 
   const init = () => {
-    const rootWidth = root.offsetWidth;
+    const $viewport: HTMLElement = root;
+    const viewportWidth = $viewport.offsetWidth;
 
     activeSnapIndex = 0;
 
-    switch (snapAlign) {
-      case 'start':
-        activeSnapObserver = new IntersectionObserver(
-          (entries) => {
-            entries.forEach((entry) => {
-              // console.log('active snap: ', entry);
+    activeSnapObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          let observer: IntersectionObserver | undefined;
 
-              if (activeSnapIndex === null) return;
-
-              const entryIndex = Array.prototype.indexOf.call(
-                children,
-                entry.target
-              );
-
-              if (entry.isIntersecting && entry.boundingClientRect.right > 0) {
-                setSnapIndex(entryIndex + 1);
-                return;
-              }
-
-              if (
-                !entry.isIntersecting &&
-                entry.intersectionRatio > 0 &&
-                entry.boundingClientRect.right > 0
-              ) {
-                setSnapIndex(entryIndex);
-                return;
-              }
-            });
-          },
-          {
-            root,
-            rootMargin: `0px -100% 0px 50%`,
-            threshold: [0.5],
+          if (entry.isIntersecting) {
+            const styles = window.getComputedStyle(
+              entry.target
+            ) as CSSStyleDeclaration;
+            const observer = getObserver(styles.scrollSnapAlign);
+            observer.observe(entry.target);
+          } else {
+            if (observer) observer.disconnect();
           }
-        );
-        break;
-      case 'end':
-        activeSnapObserver = new IntersectionObserver(
-          (entries) => {
-            entries.forEach((entry) => {
-              // console.log('active snap: ', entry);
+        });
+      },
+      {
+        root,
+        rootMargin: `0px 0px`,
+        threshold: [0.5],
+      }
+    );
 
-              if (activeSnapIndex === null || !isScrolling) return;
+    const getObserver = (snapAlign: string) => {
+      switch (snapAlign) {
+        case 'start':
+          return new IntersectionObserver(
+            (entries) => {
+              entries.forEach((entry) => {
+                console.log('active snap: ', entry);
 
-              const entryIndex = Array.prototype.indexOf.call(
-                children,
-                entry.target
-              );
+                if (activeSnapIndex === null || !entry.rootBounds) return;
 
-              if (
-                entry.isIntersecting &&
-                entry.boundingClientRect.left < rootWidth
-              ) {
-                setSnapIndex(entryIndex - 1);
-                return;
-              }
-
-              if (
-                !entry.isIntersecting &&
-                entry.intersectionRatio > 0 &&
-                entry.boundingClientRect.left < rootWidth
-              ) {
-                setSnapIndex(entryIndex);
-                return;
-              }
-            });
-          },
-          {
-            root,
-            rootMargin: `0px 50% 0px -100%`,
-            threshold: [0.5],
-          }
-        );
-        break;
-      case 'center':
-      default:
-        activeSnapObserver = new IntersectionObserver(
-          (entries) => {
-            entries.forEach((entry) => {
-              // console.log('active snap: ', entry);
-
-              if (activeSnapIndex === null || !isScrolling || !entry.rootBounds)
-                return;
-
-              if (
-                entry.intersectionRatio >= 0.49 &&
-                entry.intersectionRatio <= 0.51 &&
-                entry.boundingClientRect.left <=
-                  entry.rootBounds.left +
-                    entry.boundingClientRect.width * 0.01 &&
-                entry.boundingClientRect.right >=
-                  entry.rootBounds.left + entry.boundingClientRect.width * 0.01
-              ) {
                 const entryIndex = Array.prototype.indexOf.call(
                   children,
                   entry.target
                 );
 
-                setSnapIndex(entryIndex);
-                return;
-              }
-            });
-          },
-          {
-            root,
-            rootMargin: `0px 0px 0px -50%`,
-            threshold: [0.49, 0.51],
-          }
-        );
-        break;
-    }
+                if (
+                  entry.isIntersecting &&
+                  entry.boundingClientRect.right > 0
+                ) {
+                  setSnapIndex(entryIndex + 1);
+                  return;
+                }
+
+                if (
+                  !entry.isIntersecting &&
+                  entry.intersectionRatio > 0 &&
+                  entry.boundingClientRect.right - entry.rootBounds.right > 0
+                ) {
+                  setSnapIndex(entryIndex);
+                  return;
+                }
+              });
+            },
+            {
+              root,
+              rootMargin: `0px -100% 0px 50%`,
+              threshold: [0.5],
+            }
+          );
+        case 'end':
+          return new IntersectionObserver(
+            (entries) => {
+              entries.forEach((entry) => {
+                // console.log('active snap: ', entry);
+
+                if (activeSnapIndex === null) return;
+
+                const entryIndex = Array.prototype.indexOf.call(
+                  children,
+                  entry.target
+                );
+
+                if (
+                  entry.isIntersecting &&
+                  entry.boundingClientRect.left < viewportWidth
+                ) {
+                  setSnapIndex(entryIndex - 1);
+                  return;
+                }
+
+                if (
+                  !entry.isIntersecting &&
+                  entry.intersectionRatio > 0 &&
+                  entry.boundingClientRect.left < viewportWidth
+                ) {
+                  setSnapIndex(entryIndex);
+                  return;
+                }
+              });
+            },
+            {
+              root,
+              rootMargin: `0px 50% 0px -100%`,
+              threshold: [0.5],
+            }
+          );
+        case 'center':
+        default:
+          return new IntersectionObserver(
+            (entries) => {
+              entries.forEach((entry) => {
+                // console.log('active snap: ', entry);
+
+                if (activeSnapIndex === null || !entry.rootBounds) return;
+
+                if (
+                  entry.boundingClientRect.left <=
+                    entry.rootBounds.left +
+                      entry.boundingClientRect.width * 0.01 &&
+                  entry.boundingClientRect.right >=
+                    entry.rootBounds.left +
+                      entry.boundingClientRect.width * 0.01
+                ) {
+                  const entryIndex = Array.prototype.indexOf.call(
+                    children,
+                    entry.target
+                  );
+
+                  setSnapIndex(entryIndex);
+                  return;
+                }
+              });
+            },
+            {
+              root,
+              rootMargin: `0px 0px 0px -50%`,
+              threshold: [0.49, 0.51],
+            }
+          );
+      }
+    };
 
     root.addEventListener('scroll', handleScrolling);
 
