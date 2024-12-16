aa9018c621570e8d35bf95a5c598e59bd3899c5c
Adrien Denat <oi@adriendenat.com>
2024-12-16 15:45:46 +0000

    remove test setup until we have tests

--- packages/core/jest-unit.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.js?$': 'babel-jest'
-  },
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
-  moduleFileExtensions: ['js'],
-  setupFiles: ['<rootDir>/tests/testsSetup.js']
-};
